67425e0fffa1c6bb043bd377c3cebd08f2294aa8
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-22 10:34:39 +0200

    Initial Commit

---
+++ lesson 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 3
+//
+//  Created by Sergey on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
