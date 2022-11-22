86562acc435a9a925197fafc577b5946ed6d3b06
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-22 17:31:35 +0200

    alert

--- lesson 3/ViewController.swift
+++ lesson 3/ViewController.swift
@@ -11,26 +11,87 @@ class ViewController: UIViewController {
     
     @IBOutlet var mainLabel: UILabel!
     @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Slider
+        slider.value = 1
+        slider.minimumValue = 0// alpha view
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .black
+        slider.maximumTrackTintColor = .white
+        slider.thumbTintColor = .systemYellow
+        
+        
+        
+        
+        
         // Label
-        mainLabel.text = ""
-        mainLabel.font = mainLabel.font.withSize(35)
+   //     mainLabel.text = ""
+        mainLabel.text = String(slider.value)
+        mainLabel.font = mainLabel.font.withSize(20)
         mainLabel.numberOfLines = 2
         
        // Segmented Control
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+    
+
+        
+        
+        
+        
         
      
     }
     
 @IBAction func segmentedAction() {
-        
-        
-        
+    
+    switch segmentedControl.selectedSegmentIndex {
+            
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .orange
+            
+        case 1:
+            mainLabel.text = "The Second segment is selected"
+            mainLabel.textColor = .blue
+            
+        case 2:
+            mainLabel.text = "The Third segment is selected"
+            mainLabel.textColor = .white
+            
+            
+        default: break
+            
     }
+}
     
 
+    @IBAction func sliderAction() {
+        let currentValue = CGFloat(slider.value)
+        mainLabel.text = String(slider.value)
+        view.backgroundColor =  view.backgroundColor?.withAlphaComponent(currentValue)
+    }
+    
+    @IBAction func doneButtonPressed() {
+        
+        
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            print("Text Field is empty")
+            return
+        }
+        
+        if let _ = Double(inputText) {
+            print("Wrong format")
+            return
+        }
+    mainLabel.text = inputText
+    }
 }
 
